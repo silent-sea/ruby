2b9a25f226f69cefe794c8437e01ec288a67fffc
Amphios <vaughanslater@live.co.uk>
2016-05-18 16:09:33 +0100

    Fixed app_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  before_filter :load_hero
+
+  def load_hero
+    @hero = Hero.find_by user_id: current_user.id
+  end
 end
